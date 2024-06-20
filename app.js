a54661460882594ef1004c1302bc3c3c510047b0
Gabriela Rojas <grojas9807@gmail.com>
2024-06-19 21:36:56 -0500

    Add positive number validations

--- app.js
+++ app.js
@@ -7,16 +7,21 @@ while (!isFinite(num1)){num1 = Number(prompt("Ingrese un número válido"))}
 //* Calcular factorial
 factorial = num1 => {
   if(typeof(num1) == "number" && isFinite(num1)){
-    if (num1 > 1){
+    if (num1 >= 1){
       return num1 * factorial(num1 - 1);
     }
-    else {
+    else if (num1 == 0){
       return 1;
+    }
+    else{
+      console.log(`%cEl factorial solo se puede calcular con números positivos.`, `color: #d43926; font-size: 14px; font-weight: bold`);
+      return;
     }
   }
 }
 
-let c = 30;
 //* Imprimir por consola el resultado, con algunos detallitos de color.
-console.log(`%cResultado factorial de ${num1} es:`, `color: #c87ee8; font-size: 16px; font-weight: bold`);
-console.log(`%c-->${factorial(num1)}`, `color: #2092b6; font-size: 14px; font-weight: bold; font-style: italic`); 
+if(factorial(num1) != undefined){
+  console.log(`%cResultado factorial de ${num1} es:`, `color: #c87ee8; font-size: 16px; font-weight: bold`);
+  console.log(`%c-->${factorial(num1)}`, `color: #2092b6; font-size: 14px; font-weight: bold; font-style: italic`); 
+}
